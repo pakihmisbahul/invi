3d35f0d0cfd5afa457a7e681ef44ea9cd3a30560
Pakihmp <86660205+pakihmisbahul@users.noreply.github.com>
2024-01-21 22:19:55 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,103 @@
+// owl carousel start
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 15,
+    autoplay: true,
+    autoplayhoverpause: true,
+    autoplaytimeout: 500,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      600: {
+        items: 2,
+        dots: false,
+      },
+      1000: {
+        items: 3,
+        dots: false,
+      },
+    },
+  });
+});
+// owl carousel end
+
+// copy start
+const salin = (btn) => {
+    navigator.clipboard.writeText(btn.getAttribute('data-nomer'));
+    let tmp = btn.innerHTML;
+    btn.innerHTML = 'Tersalin';
+    btn.disabled = true;
+
+    setTimeout(() => {
+        btn.innerHTML = tmp;
+        btn.disabled = false;
+        btn.focus();
+    }, 1500);
+};
+// copy end
+
+// waktu start
+const countDownDate = new Date("Apr 19, 2024 00:00:00").getTime();
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("hari").innerHTML = days;
+  document.getElementById("jam").innerHTML = hours;
+  document.getElementById("menit").innerHTML = minutes;
+  document.getElementById("detik").innerHTML = seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Carasingkat").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// waktu end
+
+// modal start
+window.onload = function () {
+  document.getElementById("klikmodal").click();
+};
+// modal end
+
+// lagu start
+const lagu = document.getElementById("lagu");
+function playAudio() {
+  lagu.play();
+}
+function stopAudio() {
+  lagu.pause();
+}
+// lagu end
+
+// undngan start
+function GetURLParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split("&");
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split("=");
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1];
+    }
+  }
+}
+var to = GetURLParameter("to");
+document.getElementById("nama").innerHTML = to ? decodeURI(to) : "-";
+
+// hover blur effect
+$('.blur').mouseenter(function(){
+  $('.blur').css('filter','blur(5px)'); // Blurs each .blur div
+  $(this).css('filter','blur(0px)');    // Removes blur from the currently hovered .blur div
+})
+$('.blur').mouseleave(function(){
+  $('.blur').css('filter','blur(0px)'); // Removes blur from all when none are hovered
+})
+
+// undangan end
+
